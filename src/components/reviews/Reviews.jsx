bbe3a3542d6e364885eafb0c8cc8124d7a426430
tomaslinden <tomaslinden@users.noreply.github.com>
2024-09-20 16:38:34 +0300

    Add tentative support for searching for reviews

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 import ReviewCreateSelect from './ReviewCreateSelect'
 
 const Reviews = ({ subjectService, frameworkService, reviewService }) => {
@@ -14,37 +15,39 @@ const Reviews = ({ subjectService, frameworkService, reviewService }) => {
     const [reviewSelectedForDeletion, setReviewSelectedForDeletion] = useState(null)
     const [isReviewDeleteSuccess, setReviewDeleteSuccess] = useState(false)
 
-    useEffect(() => {
-        getAllReviews()
-    }, []) 
-
     const getAllReviews = () => {
         reviewService
             .getAll()
             .then(reviews => {
-                setReviews(reviews)
+                const filteredReviews = reviews.filter((review) => {
+                    return (
+                        review.subjectId === subjectId &&
+                        review.frameworkId === frameworkId
+                    )
+                })
+                setReviews(filteredReviews)
             })
     }
 
-    const handleReviewDelete = () => {
-        reviewService
-            .deleteSubject(reviewSelectedForDeletion.id)
-            .then(() => {
-                getAllReviews()
-                setReviewDeleteSuccess(true)
-                closeReviewDeletionDialog()
-                setTimeout(() => {
-                    setReviewDeleteSuccess(false)
-                    closeSubjectDeletionDialog()
-                }, 3000)
-            })
-    }
+    // const handleReviewDelete = () => {
+    //     reviewService
+    //         .deleteSubject(reviewSelectedForDeletion.id)
+    //         .then(() => {
+    //             getAllReviews()
+    //             setReviewDeleteSuccess(true)
+    //             closeReviewDeletionDialog()
+    //             setTimeout(() => {
+    //                 setReviewDeleteSuccess(false)
+    //                 closeSubjectDeletionDialog()
+    //             }, 3000)
+    //         })
+    // }
 
     const closeReviewDeletionDialog = () => {
         setShowReviewDeleteWarning(false)
         setReviewSelectedForDeletion(null)
     }
-    
+
     return (
         <>
             <h1>Reviews</h1>
@@ -62,51 +65,51 @@ const Reviews = ({ subjectService, frameworkService, reviewService }) => {
                 setFrameworkId,
                 setSelectedSubject,
                 setSelectedFramework,
+                onSelectSuccess: getAllReviews,
                 continueButtonText: 'Get reviews'
             } } />
 
-            <ul className="list-group mt-4">
-                {selectedSubject && selectedFramework && reviews.map(review => 
-                    <li className="list-group-item">
-                        <div className="d-flex justify-content-between">
-                            <div className="d-flex align-items-center">
-                                Review with id {review.id} of <strong>{review.subjectId}</strong> using <strong>{review.frameworkId}</strong>.
-                            </div>
-                            <div className="p-2"></div>
-                            <div>
-                                <button className="btn btn-primary me-md-2" style={{position: 'relative', left:"-4px"}} type="button">Modify</button>
-                                <button className="btn btn-primary" type="button">Delete</button>
-                            </div>
+            {selectedSubject && selectedFramework && reviews.map(review => {
+                return (
+                    <div> 
+                        <CardGroup key={review.id} className='mt-4'>
+                            {selectedFramework.facets.map((facet) => {
+                                return (
+                                    <Card key={facet.handle}>
+                                        <Card.Body>
+                                            <Card.Title>{facet.name}</Card.Title>
+                                            <Card.Text>
+                                                {review.facetContents.find(
+                                                    (contents) => contents.handle === facet.handle)?.contents
+                                                }
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })}
+                        </CardGroup>
+
+                        <div className='mt-2 ms-1'>
+                            <Link to={`/modifyReview/${review.id}`}>
+                                <button className="btn btn-primary me-md-2"
+                                    style={{position: 'relative', left:"-4px"}}
+                                    type="button">
+                                    Modify
+                                </button>
+                            </Link>
+
+                            <button className="btn btn-primary" type="button"
+                                onClick={() => {
+                                    setReviewSelectedForDeletion(subject)
+                                    setShowReviewDeleteWarning(true)
+                                    window.scrollTo(0, 0)
+                                }}
+                                disabled={isReviewDeleteSuccess}
+                            >Delete</button>
                         </div>
-                    </li>
-                )}
-            </ul>
-
-            <Card>
-                <Card.Body style={{display: 'flex', justifyContent: 'center'}}>
-                    This is some text within a card body.
-                    <div>
-                        {/* <Link to={`/modifyReview/${review.id}`}> */}
-                        <Link to={`/modifyReview/`}>
-                            <Button className='mt-4' variant="warning">
-                                Modify
-                            </Button>
-                        </Link>
-
-                        <Button className='mt-4 ms-2' variant="primary" onClick={() => {
-                            setReviewSelectedForDeletion(subject)
-                            setShowReviewDeleteWarning(true)
-                            window.scrollTo(0, 0)
-                            disabled={isSubjectDeleteSuccess}
-                        }}>
-                            Delete
-                        </Button>
                     </div>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>This is some text within a card body.</Card.Body>
-            </Card>
+                )
+            })}
         </>
     )
 }
