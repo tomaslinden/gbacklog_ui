7ca3ba1da0ac6b970dccd8d5a9eb71517fb6d578
tomaslinden <tomaslinden@users.noreply.github.com>
2024-09-20 20:25:37 +0300

    Add a component base for modifying reviews

---
+++ src/components/reviews/ReviewModify.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const ReviewModify = ({ reviewService }) => {
+    return (<>Modify review</>)
+}
+
+export default ReviewModify
